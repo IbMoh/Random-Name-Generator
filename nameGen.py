5c04e17db8baffab4a958baa6862a9e98995e5b4
IbMoh <115637313+IbMoh@users.noreply.github.com>
2024-04-26 00:52:47 +0200

    first commit

---
+++ nameGen.py
@@ -0,0 +1,45 @@
+import random
+import sys
+from enums import *
+
+def randomName():
+    first = random.choice(FIRSTNAME)
+    last = random.choice(LASTNAME)
+    return first + " " + last
+
+def anotherName(choice):
+    if choice == '1':
+            name = randomName()
+            print("Here's your name ")
+            print(name + "\n")
+
+def start(inputed):
+    while inputed.lower() != "x":
+        print(f"Wrong input! Inputed: {inputed}")
+        inputed = input("Press X to generate a name: ")
+    if inputed.lower() == "x":
+        name = randomName()
+        print("Here's your name " + "\n")
+        print(name + "\n")
+
+def again():
+    newName = input("Try again? 1 or 0: \n")
+    if newName == "1":
+        anotherName(newName) 
+    else:
+        print("Thanks for playing!")
+        exit()
+    
+
+def main():
+
+    print("Welcome to our random name generator!")
+    choice = input("Press X to generate a name: ")
+    start(choice)
+    
+    running = True
+    while running:
+        again()            
+            
+if __name__ == "__main__":
+    main()
